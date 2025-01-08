4f3dbb3e40f5e1b72a3d8eb7f4cd269e420a3486
Barjinder Singh <162891046+BarjnderSE@users.noreply.github.com>
2025-01-08 18:14:14 -0500

    Add files via upload

---
+++ Klinic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Clinicmgtapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
