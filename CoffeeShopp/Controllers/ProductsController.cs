aafde032b8659969331c1cd1b76df8258a4559d4
Barjinder Singh <162891046+BarjnderSE@users.noreply.github.com>
2025-01-08 17:48:50 -0500

    Add files via upload

---
+++ CoffeeShopp/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using CoffeeShopp.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IActionResult Shop()
+        {
+            return View(productRepository.GetAllProducts());
+        }
+        // need detail metjhod with oid
+        public IActionResult Details(int id)
+        {
+            var product = (productRepository.GetProductDetail(id));
+            {
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                return View(product);
+            }
+        }
+    }
+}
