3a3a25972ff212b2b38437c5ea077940e3bb8e3b
Barjinder Singh <162891046+BarjnderSE@users.noreply.github.com>
2025-01-08 17:51:05 -0500

    Add files via upload

---
+++ CoffeeShopp/Controllers/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using CoffeeShopp.Models.Interfaces;
+using CoffeeShopp.Models.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShopp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IProductRepository productRepository;
+        public HomeController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public IActionResult Index()
+        {
+            return View(productRepository.GetTrendingProducts());
+        }
+    }
+}
